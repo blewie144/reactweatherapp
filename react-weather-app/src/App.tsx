8cadb8885a35ccf3085b2c2c77ad6f0b40a4aa63
Bradley Lewis <launchpadweb@protonmail.com>
2024-12-09 10:39:15 +0800

    added footer message

--- react-weather-app/src/App.tsx
+++ react-weather-app/src/App.tsx
@@ -1,9 +1,11 @@
+import { Footer } from "./components/Footer"
 import Weather from "./components/Weather"
 
 const App = () => {
   return (
     <div className="app">
       <Weather />
+      <Footer />
     </div>
   )
 }
---
+++ react-weather-app/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+        <p>
+            Project created by 
+        <a href="https://www.bradthefreelancer.com"> bradthefreelancer</a>
+        </p>
+    </div>
+  )
+}
